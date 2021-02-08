8e80036292b58a7607473925cdc1ced0a6d60964
S11s.s.m <savanismit11@gmail.com>
2021-02-08 15:54:10 +0530

    Preview in ListContact page done

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -3,28 +3,21 @@ $("#menu-toggle").click(function(e) {
       $("#wrapper").toggleClass("toggled");
     });
 
-// Get the modal
-var modal = document.getElementById("myModal");
+function show_modal(ele){
+    id = ele.id.split("_")[1] - 1;
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+    var modal = document.getElementById("myModal_"+id);
+    modal.style.display = "block";
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close_tag")[0];
+    var span = document.getElementById("close_tag_"+id);
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
 
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
 }
